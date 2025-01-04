c2a9e3777238baf232215818d10fc8cc3e196c8e
Mark Peters <mapeters101@earthlink.net>
2025-01-04 16:26:06 -0500

    Work on lesson005.

---
+++ lesson005.py
@@ -0,0 +1,29 @@
+x = 10
+print(x)
+word = "century"
+print(word)
+
+x = 1
+y = 2
+
+word1 = "hello"
+word2 = "snow"
+
+print(x+y)
+print(word1 + " " + word2)
+#print(x+word1)
+print(str(x)+word1)
+print("=============")
+
+print(x - y)
+#print(word1 - word2)
+#print(word1 * word2)
+print(3 * "Hi")
+
+print(55.5/2)
+
+print(y ** 2)
+
+print(9 % 3)
+print(9 % 4)
+print(3 % 9)
