9c2e4654f768ed5dca2837bba45e10359a1834c2
Mark Peters <mapeters101@earthlink.net>
2025-01-04 16:43:31 -0500

    Work on lesson007.

---
+++ lesson007.py
@@ -0,0 +1,19 @@
+name = input("What is your name? ")
+print("Hello " + name)
+
+x = input("Enter an integer: ")
+y = input("Enter an integer: ")
+print(x + y)
+
+x = int(input("Enter an integer: "))
+y = int(input("Enter an integer: "))
+print(x + y)
+
+name = input("Enter your name: ")
+age = int(input("Enter you age (on the last day of this current year): "))
+
+birthYear = 2025 - age
+
+print(f"Hello {name}, you were born in {birthYear}.")
+
+print(f"Hello, my name is {name}. I was born in {birthYear}, so on New Years Eve, I am going to be {age} years old.")
