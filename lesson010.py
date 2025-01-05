d7d4c7a9c16abc2e77196b38c9856d73f93891a4
Mark Peters <mapeters101@earthlink.net>
2025-01-04 20:48:00 -0500

    Work on lesson010.

---
+++ lesson010.py
@@ -0,0 +1,14 @@
+import math
+import random
+
+print(random.randint(1, 10))
+
+#x = int(input("Enter a number: "))
+x = float(input("Enter a number: "))
+print(math.floor(x))
+print(math.ceil(x))
+
+x = int(input("Enter a number: "))
+y = int(input("Enter a number: "))
+print(math.pow(x, y))
+
