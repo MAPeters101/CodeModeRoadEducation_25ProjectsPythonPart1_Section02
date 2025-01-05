c221ba3c5b4e975bc80c05b69114cd9de57d8388
Mark Peters <mapeters101@earthlink.net>
2025-01-04 21:10:22 -0500

    Initial code for assignment04.

---
+++ assignment04.py
@@ -0,0 +1,40 @@
+#throughout this program, make sure to print your outputs in proper format.
+#ex.) Difference: x
+#Don't just print the difference, make sure you label it, so the user knows
+#what the number is that you are outputting.
+
+#import the required module(s)
+
+
+#greet the user to the project
+#ex.) Welcome to the Simple Calculator!
+
+
+
+#ask the user for two numbers, and print the sum
+
+
+#ask the user for two numbers, and print the difference
+
+
+#ask the user for two numbers, and print the product (multiplication)
+
+
+#ask the user for two numbers, and print the quotient (division)
+
+
+#ask the user for two numbers, and print the first number raised to the second
+#number (exponents using math.pow(x, y))
+
+
+#ask the user for one number, and print the absolute value (math.fabs(x))
+
+
+#ask the user for one numbers, and print the square root (math.sqrt(x))
+
+
+
+
+
+
+
