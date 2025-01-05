18d7c12fbfc9c5345c958fc3b4b028af791d0110
Mark Peters <mapeters101@earthlink.net>
2025-01-04 20:32:45 -0500

    Work on lesson009.

---
+++ lesson009.py
@@ -0,0 +1,16 @@
+x = "Hello"
+print(x[0])
+print(x[1])
+print(x[2])
+
+x = "california"
+print(x[0:3])
+print(x[:2])
+print(x[4:])
+
+
+x = "John Cena"
+print(len(x))
+print(x.upper())
+print(x.lower())
+print(x.find("c"))
